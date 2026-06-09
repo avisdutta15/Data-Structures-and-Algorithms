4680808899387d488836c0a54089ee8c543587e6
avisdutta15 <159475497+avisdutta15@users.noreply.github.com>
2026-06-09 08:33:44 +0530

    Enhance documentation for searchInRotatedSorted function
    Added detailed problem statement and explanation for the searchInRotatedSorted function. Updated comments to clarify the approach and time complexity.

--- C++/Algorithms & Patterns/3. Divide And Conquer/3. Binary Search/2. Rotated & Peak/2.SearchInSortedRotatedArray(II).cpp
+++ C++/Algorithms & Patterns/3. Divide And Conquer/3. Binary Search/2. Rotated & Peak/2.SearchInSortedRotatedArray(II).cpp
@@ -3,8 +3,50 @@
 
 using namespace std;
 
-// Time: O(logn) average case and O(n/2) worst case
-// Space: O(1)
+/*
+    Problem Statement:
+    -----------------
+    Given a sorted and rotated array (possibly with duplicates), determine if a given key 
+    exists in it, returning true if found, otherwise false.
+
+    Examples:
+    --------
+    Input: arr[] = [3, 3, 3, 1, 2, 3], key = 3 
+    Output: true
+    Explanation: 3 is present in the array.
+
+    Input: arr[] = [3, 3, 3, 1, 2, 3], key = 11 
+    Output: false
+    Explanation: 11 is not present in the given array. 
+
+    Constraints:
+    ------------
+
+    Approach:
+    ---------
+    In normal binary search, we rely on the entire array being sorted to decide whether 
+    to go left or right. But in this case, we adapt it slightly we don't require the whole 
+    array to be sorted, just identify which part is sorted in the current range. 
+    Compare A[lo] and A[mid].
+    
+    Once we know which part is sorted, we check if the target lies inside that sorted section. 
+    If it does, we discard the other half. If not, we discard the sorted half and search the 
+    remaining half. No matter how the array was rotated, the sorted structure on at least one 
+    side of any middle point always helps us narrow down where to look next. 
+    
+    This lets us avoid scanning the whole array like in brute force, and instead bring down 
+    the number of checks to logarithmic time.
+
+    NOTE : For Duplicates
+    ---------------------
+    The issue here is taking the decision of which half is sorted based on comparing A[low] and A[mid].
+    when A[low] == A[mid] == A[hi], we can't decide which half is sorted, so we shrink the search space.
+        low++; hi--; continue;
+
+    Time Complexity: O(log n) average case, O(n) worst case (when all elements are same)    
+
+*/
+
 class Solution {
 public:
     int searchInRotatedSorted(vector<int>& A, int target) {
